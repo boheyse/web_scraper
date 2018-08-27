350ab2739e858f5761664e3f33d15e119a0cead4
Bo Heyse <heyseb1@gmail.com>
2018-08-27 05:54:20 -0500

    comments added

--- scraper.py
+++ scraper.py
@@ -19,9 +19,11 @@ html_code = requests.get(url, headers=headers)
 #stores the code in a beautifulSoup object to allow for parsing
 soup = bs.BeautifulSoup(html_code.text, 'html.parser')
 
-#looks for specific html code to find average prices, varies depending on code you want to parse
+#looks for specific html code with the tag provided in find_all, varies depending on code you want to parse
 results = soup.find_all('td')
-print(results)
+
+#takes all the results and trims their tags off by converting each list value
+#to a string var, modifying that var, and then adding to a new list
 i = 0
 r = []
 while i in range(len(results)):
@@ -36,8 +38,10 @@ while i in range(len(results)):
     i+=1
 
 print(r)
+
+#creates a dataframe aka matrix so we can easily convert to a csv file
+#which is named, file.csv
 df = pd.DataFrame(data={"col1": r})
-df.iloc[4:-4]
 df.to_csv("./file.csv", sep=',',index=False)
 
 stop = timeit.default_timer()
